87511d34b60b3af728c17a55285d3ddb70d94399
365gg <wy430421@gmail.com>
2019-12-07 20:16:15 +0800

    nothing

--- app/src/main/java/com/example/spacemanhotel/MainActivity.java
+++ app/src/main/java/com/example/spacemanhotel/MainActivity.java
@@ -1,10 +1,17 @@
 package com.example.spacemanhotel;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,10 +19,19 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
     private List<Room> roomList = new ArrayList<>();
 
+    private DrawerLayout mDrawerLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar != null){
+            actionBar.setDefaultDisplayHomeAsUpEnabled(true);
+            // actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+        }
         initRooms();
         RecyclerView recyclerView = (RecyclerView)findViewById(R.id.recycler_view);
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
@@ -25,9 +41,18 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initRooms(){
-        for(int i=0;i<10;i++){
+        for(int i=0;i<15;i++){
             Room room = new Room();
             roomList.add(room);
         }
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch(item.getItemId()){
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                break;
+        }
+        return true;
     }
 }
---
+++ app/src/main/java/com/example/spacemanhotel/ReserveInfoActivity.java
@@ -0,0 +1,14 @@
+package com.example.spacemanhotel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ReserveInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reserve_info);
+    }
+}
