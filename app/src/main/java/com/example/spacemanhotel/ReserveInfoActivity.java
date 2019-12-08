9b488411a3db7922c9083ebf96db0bac3ca2f526
365gg <wy430421@gmail.com>
2019-12-08 21:37:06 +0800

    nsml

--- app/src/main/java/com/example/spacemanhotel/ReserveInfoActivity.java
+++ app/src/main/java/com/example/spacemanhotel/ReserveInfoActivity.java
@@ -5,9 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ReserveInfoActivity extends AppCompatActivity {
 
+    private List<String> dataList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,5 +24,18 @@ public class ReserveInfoActivity extends AppCompatActivity {
         if(actionBar != null){
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
+
+        dataList = new ArrayList<String>();
+        dataList.add("0");
+        dataList.add("1");
+        dataList.add("2");
+        dataList.add("3");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,dataList);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        //为spinner绑定我们定义好的数据适配器
+        Spinner spinner = (Spinner)findViewById(R.id.room_num_spinner);
+        spinner.setAdapter(adapter);
     }
 }
