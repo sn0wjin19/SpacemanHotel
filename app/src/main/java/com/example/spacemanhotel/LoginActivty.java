325217385dca087d75531aafa78cdae950a34687
365gg <wy430421@gmail.com>
2019-12-11 16:50:36 +0800

    卖身

--- app/src/main/java/com/example/spacemanhotel/LoginActivty.java
+++ app/src/main/java/com/example/spacemanhotel/LoginActivty.java
@@ -6,10 +6,26 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.text.TextPaint;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
+import android.text.style.ForegroundColorSpan;
+import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 
 import java.util.List;
 import java.util.ArrayList;
@@ -38,6 +54,32 @@ public class LoginActivty extends AppCompatActivity {
 
         Spinner spinner = (Spinner)findViewById(R.id.login_spinner_phoneAreaCode);
         spinner.setAdapter(adapter);
+
+
+//        spannable.setSpan(new ForegroundColorSpan(Color.RED), 8, 14, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        SpannableStringBuilder spannable = new SpannableStringBuilder("登录代表你已同意《卖身协议》");
+        TextView text_userAgreement = (TextView)findViewById(R.id.text_userAgreement);
+        text_userAgreement.setMovementMethod(LinkMovementMethod.getInstance());
+        spannable.setSpan(new TextClick(),8,14 , Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        text_userAgreement.setText(spannable);
+
+        Button otherWay = (Button)findViewById(R.id.login_button_otherWay);
+        otherWay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final BottomSheetDialog bottomSheet = new BottomSheetDialog(LoginActivty.this);//实例化BottomSheetDialog
+                bottomSheet.setCancelable(true);//设置点击外部是否可以取消
+                bottomSheet.setContentView(R.layout.login_dialog);//设置对框框中的布局
+                bottomSheet.findViewById(R.id.login_dialog_cancel).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+//                        Toast.makeText(LoginActivty.this, "cancel", Toast.LENGTH_SHORT).show();
+                        bottomSheet.cancel();
+                    }
+                });
+                bottomSheet.show();//显示弹窗this
+            }
+        });
     }
 
     public boolean onOptionsItemSelected(MenuItem item){
@@ -45,7 +87,27 @@ public class LoginActivty extends AppCompatActivity {
             case android.R.id.home:
                 finish();
                 break;
+            case R.id.register_item:
+                break;
         }
         return true;
     }
+
+    private class TextClick extends ClickableSpan {
+        @Override
+        public void onClick(View view) {
+            Toast.makeText(LoginActivty.this, "卖身成功", Toast.LENGTH_SHORT).show();
+        }
+        @Override
+        public void updateDrawState(TextPaint ds) {
+            ds.setColor(Color.RED);
+            ds.setUnderlineText(true);
+        }
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.register, menu);
+        return true;
+    }
+
 }
